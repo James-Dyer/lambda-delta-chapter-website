5f16f083d7713f5731c5229f30f6d325d6a4099c
James Dyer <james.dyer.dyer@gmail.com>
2025-02-04 18:02:57 -0800

    made the header modular

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../styles/header.css"; 
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-content">
+        <nav>
+          <div id="navbar-logo">
+            <a href="index.html">
+              <img src="images/LambdaDeltaLogo.png" alt="Lambda Delta Logo" />
+            </a>
+          </div>
+          <ul>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="#donate">Donate</a>
+            </li>
+            <li>
+              <a href="#events">Events</a>
+            </li>
+            <li>
+              <a href="https://sigmachi.org/" target="_blank" rel="noopener noreferrer">
+                International Fraternity
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="info">
+          <address>794 Newton Ct. Merced, CA 95348</address>
+          <a href="https://instagram.com/ucmsigmachi" target="_blank" rel="noopener noreferrer">
+            @ucmsigmachi
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -1,41 +1,12 @@
 import React from "react";
 import "../styles/home.css";
+import Header from "../components/Header"; // Import the Header component
 
 const Home = () => {
   return (
     <div>
       {/* Navbar */}
-      <header>
-        <div className="header-content">
-          <nav>
-            <div id="navbar-logo">
-              <a href="index.html">
-                <img src="images/LambdaDeltaLogo.png" alt="Lambda Delta Logo"/>
-              </a>
-            </div>
-            <ul>
-              <li>
-                <a href="index.html">Home</a>
-              </li>
-              <li>
-                <a href="#donate">Donate</a>
-              </li>
-              <li>
-                <a href="#events">Events</a>
-              </li>
-              <li>
-                <a href="https://sigmachi.org/" target="_blank" rel="noopener noreferrer">International Fraternity</a>
-              </li>
-            </ul>
-          </nav>
-          <div className="info">
-            <address>794 Newton Ct. Merced, CA 95348</address>
-            <a href="https://instagram.com/ucmsigmachi" target="_blank" rel="noopener noreferrer">
-              @ucmsigmachi
-            </a>
-          </div>
-        </div>
-      </header>
+      <Header />  {/* Modular Header Component */}
 
       {/* Main Content */}
       <main>
