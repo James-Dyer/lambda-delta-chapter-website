73463735f5fcdabc8e76c54af436ffeda8a7bc70
James Dyer <james.dyer.dyer@gmail.com>
2025-02-04 17:14:35 -0800

    creating a working react app

---
+++ sigma-chi-site/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/home.css"; // Ensure correct CSS path
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
