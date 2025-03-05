1be7aa4198a5ce5da832b113903fff7f135ff910
James Dyer <james.dyer.dyer@gmail.com>
2025-03-04 22:07:37 -0800

    content update

--- my-app/src/components/Archive.jsx
+++ my-app/src/components/Archive.jsx
@@ -10,20 +10,23 @@ const Archive = () => {
     // The URLs should be formatted as:
     // "https://drive.google.com/uc?export=view&id=YOUR_IMAGE_ID"
     const imageList = [
-      "https://drive.google.com/uc?export=view&id=IMAGE_ID_1",
-      "https://drive.google.com/uc?export=view&id=IMAGE_ID_2",
-      "https://drive.google.com/uc?export=view&id=IMAGE_ID_3",
-      "https://drive.google.com/uc?export=view&id=IMAGE_ID_4",
-      "https://drive.google.com/uc?export=view&id=IMAGE_ID_5",
-      "https://drive.google.com/uc?export=view&id=IMAGE_ID_6"
-      // add more image URLs as needed
+      "https://www.shutterstock.com/image-photo/madison-wisconsin-united-states-september-260nw-1714092871.jpg",
+      "https://c8.alamy.com/comp/EK3NC7/ann-arbor-michigan-students-at-the-university-of-michigan-gather-at-EK3NC7.jpg",
+      "https://c8.alamy.com/comp/K38EXT/old-school-front-row-from-left-to-right-jerod-mixon-as-weensie-vince-K38EXT.jpg",
+      "https://c8.alamy.com/comp/2C3PJY8/miami-march-23-2019-young-men-from-the-omega-psi-phi-fraternity-pose-for-a-photograph-at-a-spring-break-party-on-south-beach-2C3PJY8.jpg",
+      "https://media.istockphoto.com/id/473268906/photo/brothers.jpg?s=612x612&w=0&k=20&c=gAF3JiqJKA8N-ut1Tq3iEpnRJnmtvyKrRmb40IlWICo=",
+      "https://st.depositphotos.com/2619031/4756/i/450/depositphotos_47563769-stock-photo-afroamerican-and-caucasian-men-shaking.jpg",
+      "https://images.pond5.com/campus-life-students-celebrating-school-footage-036585709_iconl.jpeg",
+      "https://baylorlariat.com/wp-content/uploads/2015/03/sigma-FTW.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYqrq2ClRyVbETu2175xdeVYVsFi7F7P82eg&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuNaWxuA84FenavA23c_Z6Ha1RU9jrsBufJg&s",
     ];
     setImages(imageList);
   }, []);
 
   return (
     <div className="archive-page">
-      <h1>Archive</h1>
+      <h1>Chapter Archive</h1>
       <div className="masonry-grid">
         {images.map((url, index) => (
           <div key={index} className="masonry-item">
