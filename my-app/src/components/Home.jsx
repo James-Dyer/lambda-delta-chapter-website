339fc766826a123b49a09be1af78b350f4668504
James Dyer <james.dyer.dyer@gmail.com>
2025-04-04 19:11:18 -0700

    Home page content and spacing

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -19,10 +19,12 @@ const Home = () => {
           <div>
             <h1>A Lasting Legacy</h1>
             <p>
-            Sigma Chi was founded on June 28th, 1855 at Miami University in Oxford, Ohio.  
-            The brotherhood has only grew, as Sigma Chi 
-            recently initiated it's 350,000th member. At UC Merced, we strive to 
-            uphold our fraternity's great legacy by  ___.
+            Founded on June 28, 1855, at Miami University in Oxford, Ohio, Sigma Chi has grown into one of 
+            the largest and most respected fraternities in the world, recently initiating its 350,000th member.
+            At UC Merced, strive to uphold that great legacy by excelling in a broad range of endeavors — from 
+            impactful philanthropy to academic achievement, leadership, and campus involvement. 
+            More than just a fraternity, Sigma Chi is a lifelong brotherhood built on shared values, lasting friendships, and 
+            unforgettable experiences that extend well beyond college.
             </p>
             <p className="important-quote">
             Men join fraternities. Leaders of men join Sigma Chi.
