ce52d4fa26bc384faf2048ff2697e10bb83fe674
James Dyer <james.dyer.dyer@gmail.com>
2025-04-04 19:13:37 -0700

    Home page content

--- my-app/src/components/Home.jsx
+++ my-app/src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
             Founded on June 28, 1855, at Miami University in Oxford, Ohio, Sigma Chi has grown into one of 
             the largest and most respected fraternities in the world, recently initiating its 350,000th member.
             At UC Merced, strive to uphold that great legacy by excelling in a broad range of endeavors — from 
-            impactful philanthropy to academic achievement, leadership, and campus involvement. 
+            philanthropy to academic achievement, leadership, and campus involvement. 
             More than just a fraternity, Sigma Chi is a lifelong brotherhood built on shared values, lasting friendships, and 
             unforgettable experiences that extend well beyond college.
             </p>
