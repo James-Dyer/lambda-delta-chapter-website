8267e5d9b2043ffb1a3d6814f1c3696f3ef4bbc0
James Dyer <68835208+James-Dyer@users.noreply.github.com>
2025-06-11 17:05:20 -0700

    Add animated transitions and mobile menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion, useReducedMotion } from 'framer-motion';
 import Header from './components/Header';
 import Home from './components/Home/Home';
 import Donate from './components/Donate/Donate';
@@ -9,18 +10,43 @@ import Archive from './components/Archive';
 import Philanthropy from './components/Philanthropy';
 import Alumni from './components/Alumni';
 
+const PageWrapper = ({ children }) => {
+  const reduceMotion = useReducedMotion();
+  return (
+    <motion.div
+      {...(!reduceMotion && {
+        initial: { opacity: 0, y: 20 },
+        animate: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: -20 },
+        transition: { duration: 0.3 },
+      })}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/donate" element={<PageWrapper><Donate /></PageWrapper>} />
+        <Route path="/members" element={<PageWrapper><Members /></PageWrapper>} />
+        <Route path="/archive" element={<PageWrapper><Archive /></PageWrapper>} />
+        <Route path="/philanthropy" element={<PageWrapper><Philanthropy /></PageWrapper>} />
+        <Route path="/alumni" element={<PageWrapper><Alumni /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/donate" element={<Donate />} />
-        <Route path="/members" element={<Members />} />
-        <Route path="/archive" element={<Archive />} />
-        <Route path="/philanthropy" element={<Philanthropy />} />
-        <Route path="/alumni" element={<Alumni />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
