689a28f95244d347ac66724a170e78a203f56a9c
James Dyer <james.dyer.dyer@gmail.com>
2025-06-11 21:40:25 -0700

    Formatting and smalll animation timing adjustments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,14 @@
 // App.js
 import React from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import { AnimatePresence, motion, useReducedMotion } from 'framer-motion';
+
 import Header from './components/Header';
 import Home from './components/Home/Home';
 import Donate from './components/Donate/Donate';
@@ -15,10 +22,10 @@ const PageWrapper = ({ children }) => {
   return (
     <motion.div
       {...(!reduceMotion && {
-        initial: { opacity: 0, y: 20 },
+        initial: { opacity: 0, y: 10 },
         animate: { opacity: 1, y: 0 },
-        exit: { opacity: 0, y: -20 },
-        transition: { duration: 0.3 },
+        exit: { opacity: 0, y: -10 },
+        transition: { duration: 0.2 },
       })}
     >
       {children}
@@ -26,17 +33,63 @@ const PageWrapper = ({ children }) => {
   );
 };
 
+PageWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 const AnimatedRoutes = () => {
   const location = useLocation();
   return (
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-        <Route path="/donate" element={<PageWrapper><Donate /></PageWrapper>} />
-        <Route path="/members" element={<PageWrapper><Members /></PageWrapper>} />
-        <Route path="/archive" element={<PageWrapper><Archive /></PageWrapper>} />
-        <Route path="/philanthropy" element={<PageWrapper><Philanthropy /></PageWrapper>} />
-        <Route path="/alumni" element={<PageWrapper><Alumni /></PageWrapper>} />
+        <Route
+          path="/"
+          element={
+            <PageWrapper>
+              <Home />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/donate"
+          element={
+            <PageWrapper>
+              <Donate />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/members"
+          element={
+            <PageWrapper>
+              <Members />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/archive"
+          element={
+            <PageWrapper>
+              <Archive />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/philanthropy"
+          element={
+            <PageWrapper>
+              <Philanthropy />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/alumni"
+          element={
+            <PageWrapper>
+              <Alumni />
+            </PageWrapper>
+          }
+        />
       </Routes>
     </AnimatePresence>
   );
